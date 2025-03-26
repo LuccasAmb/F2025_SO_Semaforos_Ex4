61abc148f8ed794eca740cc5dcc35fbd33669836
LuccasAmb <ambrosio.luccas2002@gmail.com>
2025-03-26 06:50:39 -0300

    fixing

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,5 +1,8 @@
 package view;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import br.edu.fateczl.sort.MergeSort;
 import controller.Corrida;
 
@@ -10,14 +13,29 @@ public class Main {
 		Corrida[] carros = new Corrida[14];
 		
 		for(int i = 0; i < 7; i++) {
-			carros[i] = new Corrida(i, "Ferrari");
-			carros[i+1] = new Corrida(i, "Celta");
-			carros[i].start();
-			carros[i+1].start();
+			carros[i * 2] = new Corrida(i, "Ferrari");
+			carros[i * 2 + 1] = new Corrida(i, "Celta");
+			carros[i * 2].start();
+			carros[i * 2 +1].start();
 			
 		}
 		
-		MergeSort merge = new MergeSort();
+
+		for (Corrida carro : carros) {
+            try {
+                carro.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Arrays.sort(carros, Comparator.comparingInt(Corrida::getMelhorTempo));
+
+        System.out.println("\n GRID DE LARGADA:");
+        for (int i = 0; i < carros.length; i++) {
+            System.out.println((i + 1) + "º - " + carros[i].getCarro() + " da equipe #" + (carros[i].getEquipe() + 1) 
+                               + " - Melhor volta: " + carros[i].getMelhorTempo() + "s");
+        }
 		
 		
 
